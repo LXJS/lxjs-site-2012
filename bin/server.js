8580bf508beee329c75e616872ad5ebe5903111b
Tiago Rodrigues <tmcrodrigues@gmail.com>
2012-04-13 01:21:53 +0100

    again

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,10 @@
 var HTTPServer = require('http-server').HTTPServer;
 
 var httpServer = new HTTPServer({
-    root: './public/',
-    port: process.env.PORT || process.env.C9_PORT || 80
+    root: './public/'
 });
 
-httpServer.start();
+httpServer.listen(process.env.PORT || process.env.C9_PORT || 8080);
 
 process.on('SIGINT', function() {
   httpServer.log('http-server stopped.'.red);
