f7929604acd8200c6cdba8735255e5b4c6afe965
Tiago Rodrigues <tiago@top10.com>
2012-05-14 14:45:11 +0100

    oops

--- bin/build.sh
+++ bin/build.sh
@@ -15,4 +15,4 @@ cat \
   $BASEDIR/assets/js/general.js \
   >$BASEDIR/assets/js/lxjs.js
 $APPPATH/node_modules/uglify-js/bin/uglifyjs $BASEDIR/assets/js/lxjs.js > $BASEDIR/assets/js/lxjs.min.js
-/home/trodrigues/node $APPPATH/bin/replace_script_tags.js $BASEDIR/index.html
+/home/trodrigues/node/bin/node $APPPATH/bin/replace_script_tags.js $BASEDIR/index.html
